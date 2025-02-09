1422ae8b1c8a41fbcfbbc3a67cac5b33a35d93e0
Vib-UX <vibchess123@gmail.com>
2025-02-09 14:44:57 +0530

    adds cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const knowledgeBaseRoutes = require("./routes/knowledgeBaseRoutes");
 
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
